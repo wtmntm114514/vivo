d1829957fafc198dc95c9fe7eb49bfb401786d92
wtmntm114514 <108474622+wtmntm114514@users.noreply.github.com>
2022-12-14 08:42:46 +0800

    14

--- PD2196/twrp_PD2196.mk
+++ PD2196/twrp_PD2196.mk
@@ -9,7 +9,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
+# Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from PD2196 device
